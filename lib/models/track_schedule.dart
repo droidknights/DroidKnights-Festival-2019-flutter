7e15c8f7509e49282de03802a5f39e9d151403c3
Jungee <jungee9732@osstem.com>
2019-01-07 18:08:19 +0900

    Add schedule models

---
+++ lib/models/track_schedule.dart
@@ -0,0 +1,60 @@
+class ScheduleModel{
+  final String name;
+  final String title;
+  final String time;
+  final String avatarUrl;
+
+  ScheduleModel({this.name, this.title, this.time, this.avatarUrl});
+
+
+}
+List<ScheduleModel> dummyData = [
+  new ScheduleModel(
+      name: "정승욱",
+      title: "No More Fragment, No More Activity",
+      time: "10:15-11:00",
+      avatarUrl: "https://img2.nickiswift.com/img/gallery/celeb-reactions-to-time-person-of-the-year/ellen-degeneres-1512671685.jpg"
+  ),
+  new ScheduleModel(
+      name: "남상균, 정현지",
+      title: "Next Step Architecture",
+      time: "11:15-12:00",
+      avatarUrl: "https://img2.nickiswift.com/img/gallery/celeb-reactions-to-time-person-of-the-year/ellen-degeneres-1512671685.jpg"
+  ),
+  new ScheduleModel(
+      name: "한정일",
+      title: "Paging Library, 그것이 쓰고싶다",
+      time: "13:15-13:40",
+      avatarUrl: "https://img2.nickiswift.com/img/gallery/celeb-reactions-to-time-person-of-the-year/ellen-degeneres-1512671685.jpg"
+  ),
+  new ScheduleModel(
+      name: "안세원",
+      title: "지금은 ConstraintLayout 시대",
+      time: "13:55-14:40",
+      avatarUrl: "https://img2.nickiswift.com/img/gallery/celeb-reactions-to-time-person-of-the-year/ellen-degeneres-1512671685.jpg"
+  ),
+  new ScheduleModel(
+      name: "박상권",
+      title: "Firebase Dynamic Link로 사용자 유입시키기",
+      time: "14:55-15:20",
+      avatarUrl: "https://img2.nickiswift.com/img/gallery/celeb-reactions-to-time-person-of-the-year/ellen-degeneres-1512671685.jpg"
+  ),
+  new ScheduleModel(
+      name: "노재춘",
+      title: "RxJava 적용 팁과 트러블 슈팅",
+      time: "15:35-16:20",
+      avatarUrl: "https://img2.nickiswift.com/img/gallery/celeb-reactions-to-time-person-of-the-year/ellen-degeneres-1512671685.jpg"
+  ),
+  new ScheduleModel(
+      name: "정경호",
+      title: "Android Test",
+      time: "16:35-17:00",
+      avatarUrl: "https://img2.nickiswift.com/img/gallery/celeb-reactions-to-time-person-of-the-year/ellen-degeneres-1512671685.jpg"
+  ),
+  new ScheduleModel(
+      name: "강사룡",
+      title: "내가 안드로이드 개발자가 되었을 때 아무도 알려주지 않은 것들",
+      time: "17:15-18:00",
+      avatarUrl: "https://img2.nickiswift.com/img/gallery/celeb-reactions-to-time-person-of-the-year/ellen-degeneres-1512671685.jpg"
+  ),
+];
--- lib/pages/trackone_screen.dart
+++ lib/pages/trackone_screen.dart
@@ -1,12 +1,53 @@
+import 'package:droidknights/models/track_schedule.dart';
 import 'package:flutter/material.dart';
 
-class TrackOneScreen extends StatelessWidget{
+class TrackOneScreen extends StatefulWidget{
+  @override
+  TrackOneScreenState createState() {
+    return new TrackOneScreenState();
+  }
+}
+
+class TrackOneScreenState extends State<TrackOneScreen> {
   @override
   Widget build(BuildContext context) {
-    return new Center(
-      child: new Text(
-        "Track1",
-        style: new TextStyle(fontSize: 20.0),
+    return new ListView.builder(
+      itemCount: dummyData.length,
+      itemBuilder: (context, i) => new Column(
+        children: <Widget>[
+          new Divider(
+            height: 10.0,
+          ),
+          new ListTile(
+            leading: new CircleAvatar(
+              foregroundColor: Theme.of(context).primaryColor,
+              backgroundColor: Colors.grey,
+              backgroundImage: new NetworkImage(dummyData[i].avatarUrl),
+            ),
+            title: new Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                new Text(
+                  dummyData[i].title,
+                  style: new TextStyle(fontWeight: FontWeight.bold),
+                ),
+                new Text(
+                  dummyData[i].time,
+                  style: new TextStyle(color: Colors.grey, fontSize: 14.0),
+                ),
+              ],
+            ),
+            subtitle: new Container(
+              padding: const EdgeInsets.only(
+                top: 5.0,
+              ),
+              child: new Text(
+                dummyData[i].name,
+                style: new TextStyle(color: Colors.grey, fontSize: 15.0),
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
