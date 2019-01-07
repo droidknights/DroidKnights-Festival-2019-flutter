389e7af1ff192dd8e5846c753d4bad3a84eedcdc
Jungee <jungee9732@osstem.com>
2019-01-07 17:52:21 +0900

    Add tabController - track1 / track2

---
+++ lib/pages/trackone_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class TrackOneScreen extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return new Center(
+      child: new Text(
+        "Track1",
+        style: new TextStyle(fontSize: 20.0),
+      ),
+    );
+  }
+}
---
+++ lib/pages/tracktwo_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class TrackTwoScreen extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return new Center(
+      child: new Text(
+        "Track2",
+        style: new TextStyle(fontSize: 20.0),
+      ),
+    );
+  }
+}
