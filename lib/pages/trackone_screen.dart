074a65a3596d0ca8c26d0169c166f248fcfc6837
Jungee <jungee9732@osstem.com>
2019-01-08 09:28:29 +0900

    Modify Flexible - Text overflow

--- lib/pages/trackone_screen.dart
+++ lib/pages/trackone_screen.dart
@@ -1,7 +1,7 @@
 import 'package:droidknights/models/track_schedule.dart';
 import 'package:flutter/material.dart';
 
-class TrackOneScreen extends StatefulWidget{
+class TrackOneScreen extends StatefulWidget {
   @override
   TrackOneScreenState createState() {
     return new TrackOneScreenState();
@@ -14,41 +14,55 @@ class TrackOneScreenState extends State<TrackOneScreen> {
     return new ListView.builder(
       itemCount: dummyData.length,
       itemBuilder: (context, i) => new Column(
-        children: <Widget>[
-          new Divider(
-            height: 10.0,
+            children: <Widget>[
+              new Divider(
+                height: 10.0,
+              ),
+              new ListTile(
+                leading: new CircleAvatar(
+                  foregroundColor: Theme.of(context).primaryColor,
+                  backgroundColor: Colors.grey,
+                  backgroundImage: new NetworkImage(dummyData[i].avatarUrl),
+                ),
+                title: new Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    new Flexible(
+                      child: new Container(
+                        child: new Text(
+                          dummyData[i].title,
+                          maxLines: 1,
+                          overflow: TextOverflow.ellipsis,
+                          style: new TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                subtitle: new Container(
+                    padding: const EdgeInsets.only(
+                      top: 5.0,
+                    ),
+                    child: new Row(
+                      children: <Widget>[
+                        new Text(
+                          dummyData[i].name,
+                          style:
+                              new TextStyle(color: Colors.grey, fontSize: 15.0),
+                        ),
+                        new Padding(
+                            padding:
+                                const EdgeInsets.symmetric(horizontal: 5.0)),
+                        new Text(
+                          dummyData[i].time,
+                          style:
+                              new TextStyle(color: Colors.grey, fontSize: 14.0),
+                        ),
+                      ],
+                    )),
+              )
+            ],
           ),
-          new ListTile(
-            leading: new CircleAvatar(
-              foregroundColor: Theme.of(context).primaryColor,
-              backgroundColor: Colors.grey,
-              backgroundImage: new NetworkImage(dummyData[i].avatarUrl),
-            ),
-            title: new Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                new Text(
-                  dummyData[i].title,
-                  style: new TextStyle(fontWeight: FontWeight.bold),
-                ),
-                new Text(
-                  dummyData[i].time,
-                  style: new TextStyle(color: Colors.grey, fontSize: 14.0),
-                ),
-              ],
-            ),
-            subtitle: new Container(
-              padding: const EdgeInsets.only(
-                top: 5.0,
-              ),
-              child: new Text(
-                dummyData[i].name,
-                style: new TextStyle(color: Colors.grey, fontSize: 15.0),
-              ),
-            ),
-          )
-        ],
-      ),
     );
   }
 }
