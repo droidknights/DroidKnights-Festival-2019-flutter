3afdac2e6fec30c9b7ed9a1be65cc62548db0e70
jiyoung <jungee9732@station3.co.kr>
2019-02-06 14:12:27 +0900

    Add BottomNavigation & QRCode Scanner

--- lib/droidknightsapp_home.dart
+++ lib/droidknightsapp_home.dart
@@ -1,74 +1,120 @@
 import 'package:droidknights/models/Constants.dart';
 import 'package:droidknights/pages/generate_qrcode.dart';
+import 'package:droidknights/pages/scan_qrcode.dart';
+import 'package:droidknights/pages/schedule_page.dart';
 import 'package:droidknights/pages/trackone_screen.dart';
 import 'package:droidknights/pages/tracktwo_screen.dart';
+import 'package:droidknights/placeholder_widget.dart';
 import 'package:flutter/material.dart';
 
 class DroidknightsAppHome extends StatefulWidget {
   @override
   _DroidknightsAppHomeState createState() => new _DroidknightsAppHomeState();
-
 }
 
 class _DroidknightsAppHomeState extends State<DroidknightsAppHome>
-    with SingleTickerProviderStateMixin{
-
+    with SingleTickerProviderStateMixin {
   TabController _tabController;
+  int _currentIndex = 1;
+  List<Widget> _appbar = [];
+  List<Widget> _children = [];
 
   @override
   void initState() {
     super.initState();
     _tabController = new TabController(length: 3, vsync: this, initialIndex: 0);
+    _appbar.addAll([null, scheduleAppbar(), null]);
+    _children.addAll([
+      PlaceholderWidget(Colors.deepOrange),
+      scheduleBody(),
+      PlaceholderWidget(Colors.green)
+    ]);
   }
 
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      appBar: new AppBar(
-        title: new Text("DroidKnights"),
-        elevation: 0.7,
-        bottom: new TabBar(
-            controller: _tabController,
-            indicatorColor: Colors.white,
-            tabs: <Widget>[
-              new Tab(text: "Track1",),
-              new Tab(text: "Track2"),
-            ]
+        appBar: new AppBar(
+          title: new Text("DroidKnights"),
+          actions: <Widget>[
+            PopupMenuButton<String>(
+              onSelected: _select,
+              itemBuilder: (BuildContext context) {
+                return Constants.choices.map((String choice) {
+                  return PopupMenuItem<String>(
+                      value: choice, child: Text(choice));
+                }).toList();
+              },
+            ),
+          ],
+          elevation: 0.7,
+          bottom: _appbar[_currentIndex]
         ),
-        actions: <Widget>[
-          PopupMenuButton<String>(
-            onSelected: _select,
-            itemBuilder: (BuildContext context) {
-              return Constants.choices.map((String choice) {
-                return PopupMenuItem<String> (
-                  value : choice,
-                  child : Text(choice)
-                );
-              }).toList();
-            },
-          ),
-        ],
-      ),
-      body: new TabBarView(
-          controller: _tabController,
-          children: <Widget>[
-            new TrackOneScreen(),
-            new TrackTwoScreen(),
-          ]
-      ),
+        bottomNavigationBar: BottomNavigationBar(
+          onTap: onTabTapped,
+          currentIndex: _currentIndex,
+          // this will be set when a new tab is tapped
+          items: [
+            BottomNavigationBarItem(
+              icon: new Icon(Icons.info),
+              title: new Text('Info'),
+            ),
+            BottomNavigationBarItem(
+              icon: new Icon(Icons.schedule),
+              title: new Text('Schedule'),
+            ),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.person), title: Text('Profile'))
+          ],
+        ),
+        body: _children[_currentIndex]
     );
   }
-  
-  void _select(String string) {
-    if (string == Constants.QRcode) {
-      showQRcodePage(context);
-    }
-    
+
+  void onTabTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
   }
 
-  showQRcodePage(BuildContext context) {
+  void _select(String string) {
+    if (string == Constants.MakeQR) {
+      showMakeQRcodePage(context);
+    } else if (string == Constants.ReadQR) {
+      showReadQRcodePage(context);
+    }
+  }
+
+  showMakeQRcodePage(BuildContext context) {
     Navigator.of(context).push(new MaterialPageRoute(builder: (context) {
       return new GenerateScreen();
     }));
+  }
+
+  showReadQRcodePage(BuildContext context) {
+    Navigator.of(context).push(new MaterialPageRoute(builder: (context) {
+      return new ScanScreen();
+    }));
+  }
+
+  Widget scheduleAppbar() {
+    return new TabBar(
+            controller: _tabController,
+            indicatorColor: Colors.white,
+            tabs: <Widget>[
+              new Tab(
+                text: "Track1",
+              ),
+              new Tab(text: "Track2"),
+            ]);
+  }
+
+  Widget scheduleBody() {
+    return new Scaffold(
+      body: new TabBarView(controller: _tabController, children: <Widget>[
+        new TrackOneScreen(),
+        new TrackTwoScreen(),
+      ]),
+    );
   }
 }
--- lib/models/Constants.dart
+++ lib/models/Constants.dart
@@ -1,9 +1,11 @@
 class Constants {
   static const String Setting = 'Setting';
-  static const String QRcode = 'QRcode';
+  static const String MakeQR = 'Make QR';
+  static const String ReadQR = 'Read QR';
 
   static const List<String> choices = <String> [
     Setting,
-    QRcode
+    MakeQR,
+    ReadQR
   ];
 }
