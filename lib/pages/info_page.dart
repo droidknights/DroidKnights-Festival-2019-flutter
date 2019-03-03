9b29a55e9aa9cc463f291ee6632ffdad63147173
김지영 <ji9310@naver.com>
2019-03-04 02:33:04 +0900

    Modify Info page - add googleMap api

--- lib/pages/info_page.dart
+++ lib/pages/info_page.dart
@@ -1,4 +1,8 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class InfoPage extends StatefulWidget {
   @override
@@ -6,9 +10,29 @@ class InfoPage extends StatefulWidget {
 }
 
 class InfoPageState extends State<InfoPage> {
-  static const double _topSectionTopPadding = 50.0;
-  static const double _topSectionBottomPadding = 20.0;
-  static const double _topSectionHeight = 50.0;
+  static const double _sectionAllPadding = 20.0;
+  static const double _sectionTopPadding = 10.0;
+  static const double _contentsAllPadding = 10.0;
+
+
+  Completer<GoogleMapController> _controller = Completer();
+  static final CameraPosition _coexPosition = CameraPosition(
+    target: LatLng(37.513212, 127.058595),
+    zoom: 14.4746,
+  );
+
+
+  final TextStyle _titleTextStyle = new TextStyle(
+      fontSize: 19.0,
+      color: const Color(0xFF77e448),
+      fontWeight: FontWeight.w400,
+      fontFamily: "Roboto");
+
+  final TextStyle _subtitleTextStyle = new TextStyle(
+      fontSize: 13.0,
+      color: const Color(0xFFededed),
+      fontWeight: FontWeight.w200,
+      fontFamily: "Roboto");
 
   @override
   Widget build(BuildContext context) {
@@ -18,25 +42,23 @@ class InfoPageState extends State<InfoPage> {
   }
 
   _contentWidget() {
-    final bodyHeight = MediaQuery.of(context).size.height -
-        MediaQuery.of(context).viewInsets.bottom;
     return Container(
         color: const Color(0xFF112030),
         child: SingleChildScrollView(
           scrollDirection: Axis.vertical,
-          padding: const EdgeInsets.all(20.0),
+          padding: const EdgeInsets.all(_sectionAllPadding),
           child: Column(
               mainAxisAlignment: MainAxisAlignment.start,
               mainAxisSize: MainAxisSize.max,
               crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
                 new Stack(children: <Widget>[
-                  new Image.network(
-                    'https://droidknights.github.io/2019/static/media/main-graphic.b99f9fb7.png',
+                  new Image.asset(
+                    'assets/images/dk_main_graphic.png',
                     fit: BoxFit.fitWidth,
                   ),
-                  new Image.network(
-                    'https://droidknights.github.io/2019/static/media/2019_dk_title.cf69c879.png',
+                  new Image.asset(
+                    'assets/images/dk_title.png',
                     fit: BoxFit.fitWidth,
                   ),
                 ]),
@@ -49,42 +71,26 @@ class InfoPageState extends State<InfoPage> {
                       fontFamily: "Roboto"),
                 ),
                 new Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 10.0)),
+                    padding: const EdgeInsets.symmetric(vertical: _sectionTopPadding)),
                 new Text(
                   "DROID KNIGHTS 2019",
-                  style: new TextStyle(
-                      fontSize: 19.0,
-                      color: const Color(0xFF77e448),
-                      fontWeight: FontWeight.w400,
-                      fontFamily: "Roboto"),
+                  style: _titleTextStyle,
                 ),
                 new Text(
                   "드로이드나이츠는 \"안드로이드 개발자들을 위한 컨퍼런스\"를 표방하며 안드로이드 기술에 대한 세션으로 안드로이드 개발자들의 경험을 공유하고 만남의 장을 만드는 컨퍼런스 입니다.\n한국은 높은 안드로이드점유율과 관련 제조사, 플랫폼 개발회사, 앱 개발회사 등 개발 생태계가 잘 발달하였음에도 불구하고 안드로이드 개발자들이 지식을 나눌 수 있는 기회가 부족하다는 필요로 인해 비영리 개발자 조직에 의해 만들어졌습니다.\n드로이드나이츠 2017, 2018을 성공리에 개최하고 이번 2019년에는 더욱 성숙한 행사를 통해 안드로이드 개발자들이 새로운 기술과 정보를 공유하고 서로 교류할 수 있는 행사가 되기를 희망합니다.",
-                  style: new TextStyle(
-                      fontSize: 13.0,
-                      color: const Color(0xFFededed),
-                      fontWeight: FontWeight.w200,
-                      fontFamily: "Roboto"),
+                  style: _subtitleTextStyle,
                 ),
                 new Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 10.0)),
+                    padding: const EdgeInsets.symmetric(vertical: _sectionTopPadding)),
                 new Text(
                   "PROGRAM",
-                  style: new TextStyle(
-                      fontSize: 19.0,
-                      color: const Color(0xFF77e448),
-                      fontWeight: FontWeight.w400,
-                      fontFamily: "Roboto"),
+                  style: _titleTextStyle,
                 ),
                 new Text(
                   "2019 드로이드 나이츠는 세션 이외의 다양한 프로그램을 준비하고 있습니다.",
-                  style: new TextStyle(
-                      fontSize: 13.0,
-                      color: const Color(0xFFededed),
-                      fontWeight: FontWeight.w200,
-                      fontFamily: "Roboto"),
+                  style: _subtitleTextStyle,
                 ),
-                SingleChildScrollView(
+                new SingleChildScrollView(
                     scrollDirection: Axis.horizontal,
                     child: new Row(
                         mainAxisAlignment: MainAxisAlignment.start,
@@ -92,7 +98,7 @@ class InfoPageState extends State<InfoPage> {
                         crossAxisAlignment: CrossAxisAlignment.center,
                         children: <Widget>[
                           new Padding(
-                            padding: const EdgeInsets.all(10.0),
+                            padding: const EdgeInsets.all(_contentsAllPadding),
                             child: new Column(
                               mainAxisAlignment: MainAxisAlignment.start,
                               mainAxisSize: MainAxisSize.max,
@@ -108,17 +114,13 @@ class InfoPageState extends State<InfoPage> {
                                 ),
                                 new Text(
                                   "다른사람들은 어떻게 코딩할까요?\n서로 코드를 공유해봅시다.",
-                                  style: new TextStyle(
-                                      fontSize: 13.0,
-                                      color: const Color(0xFFededed),
-                                      fontWeight: FontWeight.w200,
-                                      fontFamily: "Roboto"),
+                                  style: _subtitleTextStyle,
                                 ),
                               ],
                             ),
                           ),
                           new Padding(
-                            padding: const EdgeInsets.all(10.0),
+                            padding: const EdgeInsets.all(_contentsAllPadding),
                             child: new Column(
                               mainAxisAlignment: MainAxisAlignment.start,
                               mainAxisSize: MainAxisSize.max,
@@ -134,17 +136,13 @@ class InfoPageState extends State<InfoPage> {
                                 ),
                                 new Text(
                                   "해봐야지 생각하면서 미뤄둔 기술이 있나요?\n드로이드 나이츠에서 한번 시작해보세요!",
-                                  style: new TextStyle(
-                                      fontSize: 13.0,
-                                      color: const Color(0xFFededed),
-                                      fontWeight: FontWeight.w200,
-                                      fontFamily: "Roboto"),
+                                  style: _subtitleTextStyle,
                                 ),
                               ],
                             ),
                           ),
                           new Padding(
-                            padding: const EdgeInsets.all(10.0),
+                            padding: const EdgeInsets.all(_contentsAllPadding),
                             child: new Column(
                               mainAxisAlignment: MainAxisAlignment.start,
                               mainAxisSize: MainAxisSize.max,
@@ -160,18 +158,76 @@ class InfoPageState extends State<InfoPage> {
                                 ),
                                 new Text(
                                   "당신의 개발력을 뽐내보세요!\n푸짐한 상품이 함께합니다.",
-                                  style: new TextStyle(
-                                      fontSize: 13.0,
-                                      color: const Color(0xFFededed),
-                                      fontWeight: FontWeight.w200,
-                                      fontFamily: "Roboto"),
+                                  style: _subtitleTextStyle,
                                 ),
                               ],
                             ),
                           ),
                         ])),
-
-              ]),
+                new Padding(
+                    padding: const EdgeInsets.symmetric(vertical: _sectionTopPadding)),
+                new Text(
+                  "LOCATION",
+                  style: _titleTextStyle,
+                ),
+                new Text(
+                  "장소 : 코엑스 E홀 / 서울특별시 강남구 영동대로 513 (삼성동, 코엑스)",
+                  style: _subtitleTextStyle,
+                ),
+                new Container(
+                  padding: const EdgeInsets.all(_contentsAllPadding),
+                  alignment: Alignment.topLeft,
+                  width: 300.0,
+                  height: 200.0,
+                  child: GoogleMap(
+                    mapType: MapType.normal,
+                    initialCameraPosition: _coexPosition,
+                    onMapCreated: (GoogleMapController controller) {
+                      _controller.complete(controller);
+                    },
+                  ),
+                ),
+                new Text(
+                  "SPONSOR",
+                  style: _titleTextStyle,
+                ),
+                new SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    child: new Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        mainAxisSize: MainAxisSize.max,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: <Widget>[
+                          new Padding(
+                            padding: const EdgeInsets.all(_contentsAllPadding),
+                            child: new Column(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              mainAxisSize: MainAxisSize.max,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                GestureDetector(
+                                  onTap: () => _launchURL('https://prnd.co.kr/'),
+                                  child: Image.network(
+                                    'https://droidknights.github.io/2019/static/media/%ED%97%A4%EC%9D%B4%EB%94%9C%EB%9F%AC.f213d29a.png',
+                                    width: 50.0,
+                                    fit: BoxFit.fitWidth,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ])),
+              ]
+          ),
         ));
+  }
+
+  _launchURL(url) async {
+    //const url = 'https://flutter.io';
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
   }
 }
--- lib/pages/session_detail_page.dart
+++ lib/pages/session_detail_page.dart
@@ -1,5 +1,3 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:droidknights/models/track_schedule.dart';
 
