17b7ee2ed02c221ccaef4b42239714ca7cc61e16
김지영 <ji9310@naver.com>
2019-03-09 16:38:47 +0900

    Modify Schedule ListView - apply design guide

--- lib/pages/trackone_screen.dart
+++ lib/pages/trackone_screen.dart
@@ -13,83 +13,111 @@ class TrackOneScreen extends StatefulWidget {
 
 class TrackOneScreenState extends State<TrackOneScreen> {
   List<ScheduleModel> datas;
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
         body: new FutureBuilder(
             future: loadSchedule(),
-            builder: (BuildContext context, AsyncSnapshot<List<ScheduleModel>> snapshot) {
-              if (!snapshot.hasData)
-                return new Container();
+            builder: (BuildContext context,
+                AsyncSnapshot<List<ScheduleModel>> snapshot) {
+              if (!snapshot.hasData) return new Container();
               datas = snapshot.data;
-              return new ListView.builder(
-                itemCount: datas.length,
-                itemBuilder: (context, i) =>
-                new Column(
-                  children: <Widget>[
-                    new Divider(
-                      height: 10.0,
-                    ),
-                    new ListTile(
-                      leading: new CircleAvatar(
-                        foregroundColor: Theme
-                            .of(context)
-                            .primaryColor,
-                        backgroundColor: Colors.grey,
-                        backgroundImage: new NetworkImage(datas[i].avatarUrl),
-                      ),
-                      title: new Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: <Widget>[
-                          new Flexible(
-                            child: new Container(
-                              child: new Text(
-                                datas[i].title,
-                                maxLines: 1,
-                                overflow: TextOverflow.ellipsis,
-                                style: new TextStyle(
-                                    fontWeight: FontWeight.bold),
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                      subtitle: new Container(
-                          padding: const EdgeInsets.only(
-                            top: 5.0,
-                          ),
-                          child: new Row(
-                            children: <Widget>[
-                              new Text(
-                                datas[i].name,
-                                style:
-                                new TextStyle(
-                                    color: Colors.grey, fontSize: 15.0),
-                              ),
-                              new Padding(
-                                  padding:
-                                  const EdgeInsets.symmetric(horizontal: 5.0)),
-                              new Text(
-                                datas[i].time,
-                                style:
-                                new TextStyle(
-                                    color: Colors.grey, fontSize: 14.0),
-                              ),
-                            ],
-                          )
-                      ),
-                      onTap: () => showDetailPage(context, i),
-                    ),
-                  ],
+              return new Container(
+                color: Colors.black,
+                child: new ListView.builder(
+                  itemCount: datas.length,
+                  itemBuilder: (context, i) => _itemView(context, i),
                 ),
               );
             }
-        )
+            )
+    );
+  }
+
+  Column _itemView(context, i) {
+    return new Column(
+      children: <Widget>[
+        new ListTile(
+          leading: new Text(
+            datas[i].time,
+            style: new TextStyle(color: Colors.green[300], fontSize: 12.0),
+          ),
+          title: new Container(
+            decoration: new BoxDecoration(
+                color: Colors.white,
+                borderRadius: new BorderRadius.all(
+                    const Radius.circular(4.0),
+                )
+            ),
+            padding: const EdgeInsets.all(12.0),
+            child: new Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                new CircleAvatar(
+                  maxRadius: 28.0,
+                  foregroundColor: Theme.of(context).primaryColor,
+                  backgroundColor: Colors.grey,
+                  backgroundImage: new NetworkImage(
+                      datas[i].avatarUrl,
+                  ),
+                ),
+                new Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 6.0)),
+                new Flexible(
+                  child: new Container(
+                    height: 80.0,
+                    child: new Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        new Text(
+                          datas[i].title,
+                          maxLines: 2,
+                          overflow: TextOverflow.ellipsis,
+                          style: new TextStyle(fontSize: 16.0),
+                        ),
+                        new Text(
+                          datas[i].name,
+                          style: new TextStyle(color: Colors.green[200], fontSize: 12.0),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          /*subtitle: new Container(
+              padding: const EdgeInsets.only(
+                top: 5.0,
+              ),
+              child: new Row(
+                children: <Widget>[
+                  new Text(
+                    datas[i].title,
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
+                    style: new TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  new Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 5.0)),
+                  new Text(
+                    datas[i].name,
+                    style: new TextStyle(color: Colors.grey, fontSize: 15.0),
+                  ),
+                ],
+              )
+          ),*/
+          onTap: () => showDetailPage(context, i),
+        ),
+      ],
     );
   }
 
   _getScheduleData() async {
-    String data = await DefaultAssetBundle.of(context).loadString("assets/json/schedule_track1.json");
+    String data = await DefaultAssetBundle.of(context)
+        .loadString("assets/json/schedule_track1.json");
     final jsonResult = json.decode(data);
     print(jsonResult);
   }
