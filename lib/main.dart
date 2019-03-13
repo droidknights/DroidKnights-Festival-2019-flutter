0519e4a76f7b1b5b4fc358ca6a2eb210b5f73d0f
inthewalter <21400564@handong.edu>
2019-03-13 17:11:42 +0900

    bottom tab bar에 대해 bloc 패턴 적용
    - Statefull Widget들을 Stateless Widget으로 전환
    - BlocProvider는 generic하게 구현
    - new 삭제
    - Info, Schedule Tab별로 파일 세분화 준비

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:droidknights/droidknightsapp_home.dart';
+import 'bloc/bloc_provider.dart';
+import 'bloc/tab_bloc.dart';
 
-void main() => runApp(new MyApp());
+void main() => runApp(
+  BlocProvider<TabBloc>(
+    bloc: TabBloc(),
+    child: MyApp()
+  )
+);
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
+    return  MaterialApp(
       title: "DroidKnights",
-      theme: new ThemeData(
-          primaryColor: new Color(0xff000000),
-          accentColor: new Color(0xff40d225),
-          indicatorColor: new Color(0xff40d225),
-          primaryColorLight: new Color(0xff96ce7e)
+      theme: ThemeData(
+        primaryColor: Color(0xff000000),
+        accentColor: Color(0xff40d225),
+        indicatorColor: Color(0xff40d225),
+        primaryColorLight: Color(0xff96ce7e)
       ),
       debugShowCheckedModeBanner: false,
-      home: new DroidknightsAppHome(),
+      home: DroidknightsAppHome(),
     );
   }
 }
---
+++ lib/pages/schedule_page.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class SchedulePage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
