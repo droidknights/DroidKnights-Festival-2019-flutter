47e08afa587043d1f839f1278abd7c6ca5bffbcc
Poolyum <chinapoopoo@gmail.com>
2019-03-29 19:34:38 +0900

    [UPDATE] 프로필 이미지 배경 흰색처리

--- lib/pages/session_detail_dialog.dart
+++ lib/pages/session_detail_dialog.dart
@@ -1,9 +1,9 @@
+import 'dart:io' show Platform;
 import 'dart:math';
 
 import 'package:droidknights/models/track_schedule.dart';
 import 'package:droidknights/res/strings.dart';
 import 'package:flutter/material.dart';
-import 'dart:io' show Platform;
 
 class SessionDetailDialog extends ModalRoute<void> {
   final ScheduleModel sessionData;
@@ -153,28 +153,32 @@ class SessionDetailDialog extends ModalRoute<void> {
 
   Widget get profileImage {
     return new Hero(
-      tag: sessionData,
-      child: Row(
-        children: sessionData.speakers
-            .sublist(0, min(sessionData.speakers.length, 2))
-            .map(avatarContainer)
-            .expand((w) => [w, Padding(padding: EdgeInsets.only(left: 8.0))])
-            .toList()..removeLast(),
-      )
-    );
+        tag: sessionData,
+        child: Row(
+          children: sessionData.speakers
+              .sublist(0, min(sessionData.speakers.length, 2))
+              .map(avatarContainer)
+              .expand((w) => [w, Padding(padding: EdgeInsets.only(left: 8.0))])
+              .toList()
+                ..removeLast(),
+        ));
   }
 
   Widget avatarContainer(SpeakerModel speaker) {
-    return ClipOval(
-        child: FadeInImage.assetNetwork(
-          width: avatarSize,
-          height: avatarSize,
-          fadeInDuration: const Duration(seconds: 0),
-          fadeOutDuration: const Duration(seconds: 0),
-          image: speaker.avatarUrl,
-          placeholder: Platform.isAndroid ? Strings.IMAGES_DK_PROFILE : Strings.IMAGES_DK_IOS_PROFILE,
-          fit: BoxFit.fitHeight,
-        )
+    return Container(
+      decoration: BoxDecoration(shape: BoxShape.circle, color: Colors.white),
+      child: ClipOval(
+          child: FadeInImage.assetNetwork(
+        width: avatarSize,
+        height: avatarSize,
+        fadeInDuration: const Duration(seconds: 0),
+        fadeOutDuration: const Duration(seconds: 0),
+        image: speaker.avatarUrl,
+        placeholder: Platform.isAndroid
+            ? Strings.IMAGES_DK_PROFILE
+            : Strings.IMAGES_DK_IOS_PROFILE,
+        fit: BoxFit.fitHeight,
+      )),
     );
   }
 }
